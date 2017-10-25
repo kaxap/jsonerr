6cf5663a8a26ae5d9eb7cb0497796507380efd7b
kaxapi <kashimov@gmail.com>
2017-10-25 19:13:30 +0600

    add checking err for nil

--- dao.go
+++ dao.go
@@ -5,20 +5,32 @@ package jsonerr
 
 //code 3000
 func NewGeneralDAOError(funcName string, err error, doLog bool) error {
-	return newJsonError(3000, err, "General DAO error: " + err.Error(), funcName, doLog)
+    if err == nil {
+        return nil
+    }
+    return newJsonError(3000, err, "General DAO error: " + err.Error(), funcName, doLog)
 }
 
 //code 3001
 func NewUpdateDAOError(funcName string, err error, doLog bool) error {
-	return newJsonError(3001, err, "Update DAO error: " + err.Error(), funcName, doLog)
+    if err == nil {
+        return nil
+    }
+    return newJsonError(3001, err, "Update DAO error: " + err.Error(), funcName, doLog)
 }
 
 //code 3002
 func NewCreateDAOError(funcName string, err error, doLog bool) error {
-	return newJsonError(3002, err, "Create DAO error: " + err.Error(), funcName, doLog)
+    if err == nil {
+        return nil
+    }
+    return newJsonError(3002, err, "Create DAO error: " + err.Error(), funcName, doLog)
 }
 
 //code 3003
 func NewDeleteDAOError(funcName string, err error, doLog bool) error {
-	return newJsonError(3003, err, "Delete DAO error: " + err.Error(), funcName, doLog)
+    if err == nil {
+        return nil
+    }
+    return newJsonError(3003, err, "Delete DAO error: " + err.Error(), funcName, doLog)
 }
--- serialization.go
+++ serialization.go
@@ -5,15 +5,24 @@ package jsonerr
 
 // code 1000
 func NewJsonDecodeError(funcName string, err error, doLog bool) error {
-	return newJsonError(1000, err, "JSON decode error: " + err.Error(), funcName, doLog)
+    if err == nil {
+        return nil
+    }
+    return newJsonError(1000, err, "JSON decode error: " + err.Error(), funcName, doLog)
 }
 
 // code 1001
 func NewJsonEncodeError(funcName string, err error, doLog bool) error {
-	return newJsonError(1001, err, "JSON encode error: " + err.Error(), funcName, doLog)
+    if err == nil {
+        return nil
+    }
+    return newJsonError(1001, err, "JSON encode error: " + err.Error(), funcName, doLog)
 }
 
 // code 1002
 func NewFormatError(funcName string, err error, doLog bool) error {
-	return newJsonError(1002, err, "", funcName, doLog)
+    if err == nil {
+        return nil
+    }
+    return newJsonError(1002, err, "", funcName, doLog)
 }
