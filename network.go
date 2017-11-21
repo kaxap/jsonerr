0cecd9cacd81b4ec33520d7ff06d810af6bc5cb9
kaxapi <kashimov@gmail.com>
2017-11-21 13:17:13 +0600

    add network errors

---
+++ network.go
@@ -0,0 +1,12 @@
+﻿// network errors
+// error codes start from 4000
+
+package jsonerr
+
+//code 4000
+func NewGeneralNetworkError(funcName string, err error, doLog bool) error {
+    if err == nil {
+        return nil
+    }
+    return newJsonError(4000, err, "General DAO error: " + err.Error(), funcName, doLog)
+}
